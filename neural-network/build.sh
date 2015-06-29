4e51f07d956e0d596311b895543323536ff3468c
phiresky <phiresky@users.noreply.github.com>
2015-06-29 02:37:30 +0200

    update dependencies

--- neural-network/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-touch bin/program.js
-tsc&
-while inotifywait -e close_write bin/program.js; do
-	uglifyjs --source-map bin/program.min.js.map --in-source-map bin/program.js.map --source-map-url program.min.js.map --output bin/program.min.js -m -- bin/program.js
-done
